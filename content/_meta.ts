0c3c7dac704fb2a76b2d4b57a65fb88a6b784b22
Fang Xu <xufango@gmail.com>
2026-04-26 16:02:14 -0400

    chore: clean docs metadata export

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,6 +1,8 @@
-export default {
+const meta = {
   index: "Overview",
   "hardware-build": "Hardware Build",
   "software-build": "Software Build",
   "running-instructions": "Running Instructions",
 };
+
+export default meta;
